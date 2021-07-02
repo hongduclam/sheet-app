b26121e0a12d2116f7ccb6d26c7c621b1451cadd
LamHong <lam.hong@.global>
2021-07-02 16:11:07 +0700

    Complete First Version

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3333;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
